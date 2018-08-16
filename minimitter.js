c41b6fac8ad6355f7a3de83c6d36ecb79a43877e
Mirza Brunjadze <brunjadzem@gmail.com>
2018-08-16 14:20:43 +0400

    feat: add emit function

--- minimitter.js
+++ minimitter.js
@@ -7,8 +7,16 @@ Minimitter.prototype.on = function(name, callback) {
   this.listeners[name].push(callback);
 };
 
+Minimitter.prototype.once = function(name, callback) {
+  var once = function() {
+    this.off(name, once);
+    callback.apply(this, arguments);
+  }.bind(this);
+  this.on(name, once);
+};
+
 Minimitter.prototype.off = function(name, callback) {
-  this.listeners[name] = this.listeners[name] || [];
+  if (!this.listeners[name]) return;
   var indexOfCallback = this.listeners[name].indexOf(callback);
   if (indexOfCallback > -1) {
     this.listeners[name].splice(indexOfCallback, 1);
@@ -16,8 +24,8 @@ Minimitter.prototype.off = function(name, callback) {
 };
 
 Minimitter.prototype.emit = function(name) {
+  if (!this.listeners[name]) return;
   var args = Array.prototype.slice.call(arguments, 1);
-
   this.listeners[name].forEach(function(callback) {
     callback.apply(this, args);
   });
